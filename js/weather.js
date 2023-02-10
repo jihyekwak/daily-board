5cbb630505de988e9789ff9c59bcb8a39dead9e7
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 10:00:26 -0800

    feat(Weather): display weather

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,24 @@
+const API_KEY = ""; // hide api key
+
 function getPositionSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-}
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+    fetch(URL)
+        .then(response => response.json())
+        .then((data) => {
+            const city = document.querySelector("#weather span:nth-child(1)");
+            const description = document.querySelector("#weather span:nth-child(2)");
+            const temp = document.querySelector("#weather span:nth-child(3)");
+            city.innerHTML = data.name;
+            description.innerHTML = data.weather[0].main;
+            temp.innerHTML = data.main.temp;
+        });
+};
 
 function getPositionError() {
     alert("Can't get your location")
-}
+};
 
 navigator.geolocation.getCurrentPosition(getPositionSuccess, getPositionError);
