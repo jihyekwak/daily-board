d56d2fc3594bb0058f9ee3d9dff8dabec00845f1
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 13:25:22 -0800

    Add functions to save and display username

--- js/greeting.js
+++ js/greeting.js
@@ -2,10 +2,21 @@ const loginInput = document.querySelector("#login-form input:nth-child(1)");
 const loginBtn = document.querySelector("#login-form input:nth-child(2)");
 const greeting = document.querySelector("#greeting h1");
 
+const USERNAME_KEY = "username";
+
+function printUserName(userName) {
+    greeting.innerHTML = `Hello, ${userName}`;
+}
+
+function saveUserName() {
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    printUserName(userName);
+}
+
 function handleLoginBtn(event) {
     event.preventDefault();
-    const userName = loginInput.value;
-    greeting.innerHTML = `Hello, ${userName}`;
+    saveUserName();
 }
 
 loginBtn.addEventListener("click", handleLoginBtn);
