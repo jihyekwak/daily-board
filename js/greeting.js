fc4f5e569e4a828c2013b27aaa5cad60e07df4b1
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 13:52:52 -0800

    Add hide function and refactor code

--- js/greeting.js
+++ js/greeting.js
@@ -1,22 +1,28 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input:nth-child(1)");
-const loginBtn = document.querySelector("#login-form input:nth-child(2)");
 const greeting = document.querySelector("#greeting h1");
 
 const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
 
 function printUserName(userName) {
     greeting.innerHTML = `Hello, ${userName}`;
 }
 
-function saveUserName() {
+function handleLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
     localStorage.setItem(USERNAME_KEY, userName);
     printUserName(userName);
 }
 
-function handleLoginBtn(event) {
-    event.preventDefault();
-    saveUserName();
-}
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-loginBtn.addEventListener("click", handleLoginBtn);
+if (savedUserName) {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    printUserName(savedUserName)
+} else {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleLoginSubmit);
+}
