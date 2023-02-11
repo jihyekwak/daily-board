4253e75eb65aca3fd0ed091ba47e2e2d208d555b
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 16:39:56 -0800

    Rename function and refactor code

--- js/todos.js
+++ js/todos.js
@@ -1,18 +1,23 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input:nth-child(1)");
+const todoList = document.querySelector("ul#todo-list");
+
+function printTodo(newTodo) {
+    const todoLi = document.createElement("li");
+    const todoSpan = document.createElement("span");
+    todoSpan.innerHTML = newTodo;
+    const todoDeleteBtn = document.createElement("button");
+    todoDeleteBtn.innerHTML = "x";
+    todoLi.appendChild(todoSpan);
+    todoLi.appendChild(todoDeleteBtn);
+    todoList.appendChild(todoLi);
+}
 
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
-    const todoUl = document.querySelector("ul#todo-list")
-    const todoLi = document.createElement("li");
-    const todoSpan = document.createElement("span");
-    todoUl.appendChild(todoLi);
-    todoLi.appendChild(todoSpan)
-    todoSpan.innerHTML = newTodo;
-    const todoDeleteBtn = document.createElement("button");
-    todoLi.appendChild(todoDeleteBtn);
-    todoDeleteBtn.innerHTML = "x";
+    todoInput.value = "";
+    printTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
