1eb2329f8e848f5f0b29f520c8ff3efcd4ef25ac
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 16:50:28 -0800

    delete todo

--- js/todos.js
+++ js/todos.js
@@ -2,6 +2,18 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input:nth-child(1)");
 const todoList = document.querySelector("ul#todo-list");
 
+const todos = [];
+const TODOS_KEY = "todos"
+
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, todos);
+}
+
+function deleteTodo(event) {
+    const deletedTodoLi = event.target.parentElement;
+    deletedTodoLi.remove();
+}
+
 function printTodo(newTodo) {
     const todoLi = document.createElement("li");
     const todoSpan = document.createElement("span");
@@ -11,12 +23,15 @@ function printTodo(newTodo) {
     todoLi.appendChild(todoSpan);
     todoLi.appendChild(todoDeleteBtn);
     todoList.appendChild(todoLi);
+    todoDeleteBtn.addEventListener("click", deleteTodo);
 }
 
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    todos.push(newTodo);
+    saveTodos();
     printTodo(newTodo);
 }
 
