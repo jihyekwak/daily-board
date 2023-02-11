af766784bd4ca7e996bb6e5cad89ff730d442b9a
jihyekwak <jihyekwak53@gmail.com>
2023-02-10 16:08:18 -0800

    display todos

--- js/todos.js
+++ js/todos.js
@@ -4,7 +4,15 @@ const todoInput = document.querySelector("#todo-form input:nth-child(1)");
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
-    console.log(newTodo);
+    const todoUl = document.querySelector("ul#todo-list")
+    const todoLi = document.createElement("li");
+    const todoSpan = document.createElement("span");
+    todoUl.appendChild(todoLi);
+    todoLi.appendChild(todoSpan)
+    todoSpan.innerHTML = newTodo;
+    const todoDeleteBtn = document.createElement("button");
+    todoLi.appendChild(todoDeleteBtn);
+    todoDeleteBtn.innerHTML = "x";
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
