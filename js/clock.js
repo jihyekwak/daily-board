026827e31e02e7cecd92812572fc281ae20e2f83
jihyekwak <jihyekwak53@gmail.com>
2023-02-14 14:24:33 -0800

    Update style

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("#clock span");
+const greeting = document.querySelector("#greeting span:first-child");
 
 function showClock() {
     const date = new Date();
@@ -10,3 +11,12 @@ function showClock() {
 
 showClock();
 setInterval(showClock, 1000);
+
+const hours = new Date().getHours();
+if ( hours < 12 ) {
+    greeting.innerHTML = "Good morning,";
+} else if ( hours > 18) {
+    greeting.innerHTML = "Good evening,"
+} else {
+    greeting.innerHTML = "Good afternoon,"
+}
