194a01a92357e73b3c27fb332376c539400d0c6e
jihyekwak <jihyekwak53@gmail.com>
2023-02-14 13:45:49 -0800

    Add weather icon and update style

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,8 @@ function showClock() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+    // const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = `${hours}:${minutes}`;
 }
 
 showClock();
--- js/weather.js
+++ js/weather.js
@@ -3,17 +3,19 @@ const API_KEY = ""; // hide api key
 function getPositionSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
     fetch(URL)
         .then(response => response.json())
         .then((data) => {
-            const city = document.querySelector("#weather span:nth-child(1)");
+            const icon = document.querySelector("#weather img");
             const description = document.querySelector("#weather span:nth-child(2)");
             const temp = document.querySelector("#weather span:nth-child(3)");
-            city.innerHTML = data.name;
+            const city = document.querySelector("#weather span:nth-child(4)");
+            icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             description.innerHTML = data.weather[0].main;
-            temp.innerHTML = data.main.temp;
+            temp.innerHTML = `${data.main.temp}℃`;
+            city.innerHTML = `@${data.name}`;
         });
 };
 
